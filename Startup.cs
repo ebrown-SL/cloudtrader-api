b4564d4ea5761940a43c87267d8371aa1c9283d1
Tilly Woodfield <tillyrwoodfield@gmail.com>
2020-06-09 15:23:58 +0100

    Remove default comments

--- Startup.cs
+++ Startup.cs
@@ -22,7 +22,6 @@ namespace CloudTrader.Api
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<DataContext>(options => options.UseInMemoryDatabase(databaseName: "Users"));
@@ -65,7 +64,6 @@ namespace CloudTrader.Api
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
