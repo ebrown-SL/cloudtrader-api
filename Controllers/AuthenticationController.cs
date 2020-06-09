1ee643dc82156fcfde75aa787496266955b5747f
Tilly Woodfield <tillyrwoodfield@gmail.com>
2020-06-09 10:41:43 +0100

    Create Authentication model, add required fields, return model

---
+++ Controllers/AuthenticationController.cs
@@ -0,0 +1,16 @@
+﻿using CloudTrader.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CloudTrader.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthenticationController : Controller
+    {
+        [HttpPost("register")]
+        public ActionResult Register(AuthenticationModel model)
+        {
+            return Ok(model);
+        }
+    }
+}
---
+++ Models/AuthenticationModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CloudTrader.Api.Models
+{
+    public class AuthenticationModel
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
