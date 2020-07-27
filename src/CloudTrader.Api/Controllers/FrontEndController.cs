1c1bffd883c2a48da62fceaf5e75d75d3ade876d
Laura Warren <lwarren@scottlogic.com>
2020-07-27 12:20:52 +0100

    Create FrontEndController and expose endpoint for getting User details fro userId

---
+++ src/CloudTrader.Api/Controllers/FrontEndController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CloudTrader.Api.Service.Models;
+using CloudTrader.Api.Data;
+using CloudTrader.Api.Service.Interfaces;
+
+namespace CloudTrader.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FrontEndController : Controller
+    {
+        private readonly IUserRepository _userRepository;
+
+        public FrontEndController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        [HttpPost("getUser{id}")]
+        public async Task<User> GetUser(int id)
+        {
+            var user = await _userRepository.GetUser(id);
+            return user;
+        }
+
+        
+    }
+}
